d179b88f5ec0da6bd84d7a5118375c18d417e162
Denis Makarchuk <hiedmant@gmail.com>
2019-08-28 15:18:45 +0300

    [+] KindOfSushi & Hello in Speak

---
+++ Aducational_Project/Sushi_Order/EnumsSushi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sushi_Order
+{
+    class EnumsSushi
+    {
+        public enum Rolls
+        {
+
+        }
+
+        public enum Nigiri
+        {
+
+        }
+
+        public enum Gunkans
+        {
+
+        }
+    }
+}
---
+++ Aducational_Project/Sushi_Order/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sushi_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Speak.Hello();
+
+            Console.Read();
+        }
+    }
+}
