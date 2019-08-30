0320350ec3897276b6859492391234bfd22cbe28
Denis Makarchuk <hiedmant@gmail.com>
2019-08-30 20:53:41 +0300

    [+] SushiRepo

---
+++ Aducational_Project/SushiMenu/ISushiRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SushiMenu
+{
+    interface ISushiRepository
+    {
+        void CreateSushi(Sushi sushi);
+
+        Sushi GetSushiByName(string name);
+
+        List<Sushi> GetSushis();
+
+        void UpdateSushi(Sushi sushi);
+
+        void DeleteSushi(string name);
+    }
+}
---
+++ Aducational_Project/Sushi_Order/ISushiRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sushi_Order
+{
+    interface ISushiRepository
+    {
+        void CreateSushi(Sushi sushi);
+
+        Sushi GetSushiByID(int id);
+
+        List<Sushi> Sushis();
+
+        Sushi UpdateSushi(Sushi moto);
+
+        void DeleteSushi(int id);
+    }
+}
